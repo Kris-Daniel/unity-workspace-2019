91808a3e114d8c413ecf198402e8342111b6bf9b
krisdanieltmg <67691729+krisdanieltmg@users.noreply.github.com>
2020-10-07 18:52:58 +0300

    Add raycast for leg

---
+++ workspace-2019/Assets/Scripts/ArmRayController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmRayController : MonoBehaviour
+{
+    [SerializeField] Transform ArmRayFrom;
+    [SerializeField] Transform ArmRayPoint;
+
+    int _layerMask;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _layerMask = LayerMask.GetMask("Ground");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        
+        if (Physics.Raycast(ArmRayFrom.position, ArmRayFrom.TransformDirection(Vector3.down), out hit, Mathf.Infinity, _layerMask))
+        {
+            Debug.DrawRay(ArmRayFrom.position, ArmRayFrom.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
+            ArmRayPoint.position = hit.point;
+        }
+    }
+}
