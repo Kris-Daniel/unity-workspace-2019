5a7b1f513e345d721e68fb54f7503ec3d70a2062
krisdanieltmg <67691729+krisdanieltmg@users.noreply.github.com>
2020-10-08 13:56:47 +0300

    Improve leg movements

--- workspace-2019/Assets/RayPoints.cs
+++ workspace-2019/Assets/RayPoints.cs
@@ -15,6 +15,7 @@ public class RayLegData
 
 public class RayPoints : MonoBehaviour
 {
+    [SerializeField] Transform midPointObject;
     [SerializeField] List<RayLegData> rayLegDataList;
     bool _inMoveProcess;
     int _layerMask;
@@ -29,26 +30,65 @@ public class RayPoints : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        midPointObject.position = GetMidPoint();
+
+        var clearedBodyPos = transform.position;
+        clearedBodyPos.y = midPointObject.position.y;
+        
+        var direction = clearedBodyPos.x - midPointObject.position.x;
+        
         if(_inMoveProcess) return;
+        
+        bool inMoveProcess = false;
         
         for (int i = 0; i < rayLegDataList.Count; i++)
         {
-            _inMoveProcess = ArmRayCast(rayLegDataList[i]);
-            if (_inMoveProcess)
-            {
-                StartCoroutine(ForwardMove(i));
-                break;
-            }
+            inMoveProcess = ArmRayCast(rayLegDataList[i]) || inMoveProcess;
         }
+        
+        _inMoveProcess =  inMoveProcess;
+        if (_inMoveProcess)
+        {
+            var moveRoutine = direction > 0 ? ForwardMove(0) : BackMove(1);
+            StartCoroutine(moveRoutine);
+        }
+    }
+
+    Vector3 GetMidPoint()
+    {
+        Vector3[] midPoints = new Vector3[2];
+        for (int i = 0, j = 0; i < rayLegDataList.Count; i += 2, j++)
+        {
+            midPoints[j] = (rayLegDataList[i].legTarget.position + rayLegDataList[i + 1].legTarget.position) / 2f;
+        }
+        
+        Vector3 midPoint = (midPoints[0] + midPoints[1]) / 2f;
+
+        return midPoint;
     }
 
     IEnumerator ForwardMove(int i)
     {
         for (int j = 0; j < 4; j++)
         {
-            rayLegDataList[i].legTarget.DOMove(rayLegDataList[i].rayHitObject.position, 0.1f);
+            var direction = rayLegDataList[i].rayHitObject.position - rayLegDataList[i].legTarget.position;
+            rayLegDataList[i].legTarget.DOMove(direction * 1.5f, 0.1f).SetRelative();
             i++;
             i = i >= 4 ? 0 : i;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        _inMoveProcess = false;
+    }
+    
+    IEnumerator BackMove(int i)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            var direction = rayLegDataList[i].rayHitObject.position - rayLegDataList[i].legTarget.position;
+            rayLegDataList[i].legTarget.DOMove(direction * 1.5f, 0.1f).SetRelative();
+            i--;
+            i = i < 0 ? 3 : i;
             yield return new WaitForSeconds(0.1f);
         }
 
@@ -66,6 +106,6 @@ public class RayPoints : MonoBehaviour
         
         float distance = Vector3.Distance(rayLegData.rayHitObject.position, rayLegData.legTarget.position);
 
-        return distance > 0.3f;
+        return distance > 0.5f;
     }
 }
